79f35cc430fbe8e973e9341dcba2a7ae01519e29
ionutgit <ionut_chirita@yahoo.com>
2024-04-05 13:34:17 +0300

    components design patterns: separtion of concerns

--- src/components/EmailInput.vue
+++ src/components/EmailInput.vue
@@ -4,24 +4,25 @@
         class="form-control"
         placeholder="E-mail"
         :value="modelValue"
-        @input="updateValue"
+        @input="updateValue($event)"
         :class="{
-            'is-valid':
-            modelValue.length >= 4 && modelValue.length > 0,
-            'is-invalid':
-            modelValue.length < 4 && modelValue.length > 0,
+            'is-valid': validateEmail(modelValue),
+            'is-invalid': !validateEmail(modelValue),
         }"
     />
 </template>
 
 <script>
+import validateEmail from '../validate-email';
+
 export default {
     name: "EmailInput", 
     props: ['modelValue'],
     methods: {
-        updateValue(event) {
-            this.$emit('update:modelValue', event.target.value)
-        }
+        updateValue($event) {
+            this.$emit('update:modelValue', $event.target.value)
+        }, 
+        validateEmail
     }
 }
 </script>
